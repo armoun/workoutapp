6b9b834345e8e96309dae7227e1f650bde9db1d4
NielsLammens <lammens.niels@gmail.com>
2014-10-22 12:28:28 +0200

    Basis database helper

--- WorkoutApp/app/src/main/java/be/howest/nmct3/workoutapp/data/DatabaseHelper.java
+++ WorkoutApp/app/src/main/java/be/howest/nmct3/workoutapp/data/DatabaseHelper.java
@@ -1,7 +1,64 @@
 package be.howest.nmct3.workoutapp.data;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by nielslammens on 22/10/14.
  */
-public class DatabaseHelper {
+public class DatabaseHelper extends SQLiteOpenHelper{
+
+    public static DatabaseHelper INSTANCE;
+    public static Object lock = new Object();
+
+    public static final String DB_NAME = "workoutapp.db";
+    public static final int DB_VERSION = 1;
+
+    public DatabaseHelper(Context context){super(context, DB_NAME, null, DB_VERSION);}
+
+    public static DatabaseHelper getInstance(Context context) {
+        if(INSTANCE == null){
+            synchronized (lock){
+                if(INSTANCE == null){
+                    INSTANCE = new DatabaseHelper(context.getApplicationContext());
+                }
+            }
+        }
+
+        return INSTANCE;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE " + Contract.Workouts.CONTENT_DIRECTORY);
+        db.execSQL("DROP TABLE " + Contract.WorkoutExercises.CONTENT_DIRECTORY);
+
+        createWorkoutsTableV1(db);
+        createWorkoutExercisesTableV1(db);
+    }
+
+    private void createWorkoutsTableV1(SQLiteDatabase db){
+        String SQL = "CREATE TABLE " + Contract.Workouts.CONTENT_DIRECTORY + " ("
+                + Contract.WorkoutColumns._ID       + " INTEGER PRIMARY KEY, "
+                + Contract.WorkoutColumns.NAME      + " TEXT,"
+                + Contract.WorkoutColumns.ISPAID    + " INTEGER"
+                + ");";
+
+        db.execSQL(SQL);
+    }
+
+    private void createWorkoutExercisesTableV1(SQLiteDatabase db){
+        String SQL = "CREATE TABLE " + Contract.WorkoutExercises.CONTENT_DIRECTORY + " ("
+                + Contract.WorkoutExerciseColumns._ID           + " INTEGER PRIMARY KEY, "
+                + Contract.WorkoutExerciseColumns.WORKOUT_ID    + " INTEGER, "
+                + Contract.WorkoutExerciseColumns.EXERCISE_ID   + " INTEGER, "
+                + Contract.WorkoutExerciseColumns.REPS          + " TEXT"
+                + ");";
+    }
 }
