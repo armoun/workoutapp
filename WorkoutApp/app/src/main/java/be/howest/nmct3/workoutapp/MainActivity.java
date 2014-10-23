6c00c976a204c09e7d80cf739a29eb9fb92d3169
ViktorDB <Viktor@Viktors-MacBook-Pro.local>
2014-10-23 22:56:12 +0200

    Drawer update + first layout dashboard
    het icoontje dat eerst niet wou meebewegen van de drawer + layout van
    dashboard (moet nog afgewerkt worden)

--- WorkoutApp/app/src/main/java/be/howest/nmct3/workoutapp/MainActivity.java
+++ WorkoutApp/app/src/main/java/be/howest/nmct3/workoutapp/MainActivity.java
@@ -1,6 +1,7 @@
 package be.howest.nmct3.workoutapp;
 
-import android.app.Activity;
+
+import android.support.v4.app.FragmentManager;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
@@ -20,11 +21,12 @@ public class MainActivity extends FragmentActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
-    private CharSequence mDrawerTitle;
+    private ListView mDrawerList;
     private CharSequence mTitle;
 
     ActionBarDrawerToggle icon;
-    final String[] listContent ={"Dashboard","Exercises","Workouts","Planner","Settings"};
+
+    final String[] listTitles ={"Dashboard","Exercises","Workouts","Planner","Settings"};
     final String[] fragments ={
             "be.howest.nmct3.workoutapp.DashboardFragment",
             "be.howest.nmct3.workoutapp.ExercisesFragment",
@@ -37,35 +39,20 @@ public class MainActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTitle = mDrawerTitle = getTitle();
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.drawer);
 
-        ArrayAdapter<String> ad = new ArrayAdapter<String>(getActionBar().getThemedContext(), android.R.layout.simple_list_item_1, listContent);
+        //set adapter
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listTitles));
 
-        final DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
-        final ListView list = (ListView) findViewById(R.id.drawer);
+        //set list click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
-        FragmentTransaction transition = getSupportFragmentManager().beginTransaction();
-        transition.replace(R.id.main, Fragment.instantiate(MainActivity.this, fragments[0]));
-        getActionBar().setTitle(listContent[0]);
-        transition.commit();
-
-        list.setAdapter(ad);
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
-                drawer.setDrawerListener( new DrawerLayout.SimpleDrawerListener(){
-                    @Override
-                    public void onDrawerClosed(View drawerView){
-                        super.onDrawerClosed(drawerView);
-                        FragmentTransaction transition = getSupportFragmentManager().beginTransaction();
-                        transition.replace(R.id.main, Fragment.instantiate(MainActivity.this, fragments[position]));
-                        getActionBar().setTitle(listContent[position]);
-                        transition.commit();
-                    }
-                });
-                drawer.closeDrawer(list);
-            }
-        });
+        // Create and set the start fragment (Dashboard)
+        Fragment frag = Fragment.instantiate(MainActivity.this, fragments[0]);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.main, frag).commit();
+        getActionBar().setTitle(listTitles[0]);
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerToggle = new ActionBarDrawerToggle(
@@ -79,13 +66,13 @@ public class MainActivity extends FragmentActivity {
             /** Called when a drawer has settled in a completely closed state. */
             public void onDrawerClosed(View view) {
                 super.onDrawerClosed(view);
-
+                invalidateOptionsMenu();
             }
 
             /** Called when a drawer has settled in a completely open state. */
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
-                getActionBar().setTitle(mDrawerTitle);
+                invalidateOptionsMenu();
             }
         };
 
@@ -95,6 +82,35 @@ public class MainActivity extends FragmentActivity {
         getActionBar().setHomeButtonEnabled(true);
 
     }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    /** Swaps fragments in the main content view */
+    private void selectItem(int position) {
+
+        // Create and set a new fragment
+        Fragment frag = Fragment.instantiate(MainActivity.this, fragments[position]);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.main, frag).commit();
+
+
+        // Highlight the selected item, update the title, and close the drawer
+        mDrawerList.setItemChecked(position, true);
+        setTitle(listTitles[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getActionBar().setTitle(mTitle);
+    }
+
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
@@ -109,12 +125,14 @@ public class MainActivity extends FragmentActivity {
         mDrawerToggle.syncState();
     }
 
+    //creates the items on action bar
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.my, menu);
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -131,4 +149,15 @@ public class MainActivity extends FragmentActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    //hides the action menu icons when drawer is open
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content view
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+        menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+
 }
