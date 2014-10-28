3ae3af5cc3ba585374702ac81126f55f11ff0441
NielsLammens <lammens.niels@gmail.com>
2014-10-28 13:31:01 +0100

    WorkoutsLoader en WorkoutDatasource
    Loader haalt asynchroon de workouts op
    Datasource om nieuwe workout toe te voegen aan de db

---
+++ WorkoutApp/app/src/main/java/be/howest/nmct3/workoutapp/data/WorkoutDatasoure.java
@@ -0,0 +1,52 @@
+package be.howest.nmct3.workoutapp.data;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.util.Log;
+
+import java.util.List;
+
+/**
+ * Created by nielslammens on 27/10/14.
+ */
+public class WorkoutDatasoure {
+
+    public void createWorkout(Context context, String name, Integer isPaid, List<Exercise> exercises){
+
+        ContentValues values1 = new ContentValues();
+        values1.put(Contract.Workouts.NAME, name);
+        values1.put(Contract.Workouts.ISPAID, isPaid);
+
+        Uri uri = context.getContentResolver().insert(Contract.Workouts.CONTENT_URI, values1);
+
+        Cursor c = context.getContentResolver().query(uri, null,null,null,null);
+        c.moveToFirst();
+
+        Log.d("be.howest.nmct3.workoutapp", uri.toString() + " " + c.getString(c.getColumnIndex(c.getColumnName(0))) + ";" + c.getString(c.getColumnIndex(c.getColumnName(1))) + ";" + c.getString(c.getColumnIndex(c.getColumnName(2))));
+
+        Integer workout_id = c.getInt(c.getColumnIndex(c.getColumnName(0)));
+
+        for (int i = 0; i < exercises.size(); i++) {
+            ContentValues values2 = new ContentValues();
+            values2.put(Contract.WorkoutExercises.WORKOUT_ID, workout_id);
+            values2.put(Contract.WorkoutExercises.EXERCISE_ID, exercises.get(i).Id);
+            values2.put(Contract.WorkoutExercises.REPS, repsToString(exercises.get(i).Reps));
+        }
+
+    }
+
+    public String repsToString(List<Integer> repList){
+        String reps = "";
+        for (int i = 0; i < repList.size(); i++) {
+            reps += repList.get(i);
+            reps += ",";
+        }
+
+        reps = reps.substring(0, reps.length() - 1);
+
+        return reps;
+    }
+
+}
---
+++ WorkoutApp/app/src/main/java/be/howest/nmct3/workoutapp/data/WorkoutsLoader.java
@@ -0,0 +1,104 @@
+package be.howest.nmct3.workoutapp.data;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v4.content.AsyncTaskLoader;
+
+/**
+ * Created by nielslammens on 28/10/14.
+ */
+public class WorkoutsLoader extends AsyncTaskLoader<Cursor> {
+
+    private Cursor mData;
+
+    public WorkoutsLoader(Context context) {
+        super(context);
+    }
+
+    @Override
+    public Cursor loadInBackground() {
+        DatabaseHelper helper = DatabaseHelper.getInstance(getContext());
+
+        SQLiteDatabase db = helper.getReadableDatabase();
+
+        mData = db.query(Contract.Workouts.CONTENT_DIRECTORY,
+                new String[]{
+                        Contract.WorkoutColumns._ID,
+                        Contract.WorkoutColumns.NAME,
+                        Contract.WorkoutColumns.ISPAID,
+                        "COUNT(*) AS " + Contract.WorkoutColumns._COUNT},
+                null,
+                null,
+                null,
+                null,
+                Contract.WorkoutColumns.ISPAID + " ASC"
+        );
+
+        mData.getCount();
+
+        return mData;
+    }
+
+    @Override
+    public void deliverResult(Cursor data) {
+        if(isReset()){
+            if(data != null){
+                data.close();
+            }
+            return;
+        }
+
+        Cursor oldData = mData;
+        mData = data;
+
+        if(isStarted()){
+            super.deliverResult(data);
+        }
+
+        if ((oldData != null && oldData != data && !oldData.isClosed())){
+            oldData.close();
+        }
+
+    }
+
+    @Override
+    protected void onStartLoading() {
+        if(mData != null){
+            deliverResult(mData);
+        }
+
+        if(takeContentChanged() || mData == null){
+            forceLoad();
+        }
+    }
+
+    @Override
+    protected void onStopLoading() {
+        cancelLoad();
+    }
+
+    @Override
+    public void onCanceled(Cursor data) {
+        if(data != null && !data.isClosed()){
+            data.close();
+        }
+    }
+
+    @Override
+    protected void onReset() {
+        super.onReset();
+
+        onStopLoading();
+
+        if(mData != null && !mData.isClosed()){
+            mData.close();
+        }
+
+        mData = null;
+    }
+
+    private void releaseResources(Cursor data){
+        data.close();
+    }
+}
