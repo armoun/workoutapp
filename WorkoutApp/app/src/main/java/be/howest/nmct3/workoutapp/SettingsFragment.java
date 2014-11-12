b67bb9a6ff63f3e644552566b265c722af1c332c
ViktorDB <Viktor@Viktors-MacBook-Pro.local>
2014-11-12 12:08:37 +0100

    list workouts select exercises

--- WorkoutApp/app/src/main/java/be/howest/nmct3/workoutapp/SettingsFragment.java
+++ WorkoutApp/app/src/main/java/be/howest/nmct3/workoutapp/SettingsFragment.java
@@ -3,11 +3,20 @@ package be.howest.nmct3.workoutapp;
 
 
 import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 
 /**
@@ -15,6 +24,8 @@ import android.view.ViewGroup;
  *
  */
 public class SettingsFragment extends Fragment {
+
+    private ListAdapter myListAdapter;
 
 
     public SettingsFragment() {
@@ -31,8 +42,34 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.settings_fragment_layout, null);
+
+        myListAdapter = new CustomSettingsAdapter();
+        ListView listview = (ListView) root.findViewById(R.id.settings_list);
+        listview.setAdapter(myListAdapter);
+
         return root;
     }
 
+    class CustomSettingsAdapter extends ArrayAdapter<String> {
+
+        public CustomSettingsAdapter() {
+            super(getActivity(), R.layout.settings_list_item_layout, R.id.list_settings_item_text);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View row = super.getView(position, convertView, parent);
+
+            TextView txtNavDrawerTitle = (TextView) row.findViewById(R.id.list_settings_item_text);
+            txtNavDrawerTitle.setText("test");
+
+            return row;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+    }
 
 }
