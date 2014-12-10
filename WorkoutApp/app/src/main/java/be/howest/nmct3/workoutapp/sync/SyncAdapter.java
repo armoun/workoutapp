0e14523d0976fa443159a5ff9e021a6f7fcb69a0
NielsLammens <lammens.niels@gmail.com>
2014-12-10 10:58:59 +0100

    update sync planner

--- WorkoutApp/app/src/main/java/be/howest/nmct3/workoutapp/sync/SyncAdapter.java
+++ WorkoutApp/app/src/main/java/be/howest/nmct3/workoutapp/sync/SyncAdapter.java
@@ -1,21 +1,15 @@
 package be.howest.nmct3.workoutapp.sync;
 
 import android.accounts.Account;
-import android.accounts.AccountManager;
-import android.accounts.AccountManagerFuture;
 import android.content.AbstractThreadedSyncAdapter;
 import android.content.ContentProviderClient;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
-import android.content.OperationApplicationException;
 import android.content.SyncResult;
 import android.database.Cursor;
-import android.database.MatrixCursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.RemoteException;
-import android.provider.BaseColumns;
 import android.util.JsonReader;
 import android.util.Log;
 
@@ -53,6 +47,7 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
     private static final String FEED_URL_WORKOUTS = "http://www.viktordebock.be/mad_backend/api/workoutsbyuser/index.php?username=viktordebock";
     private static final String UPLOAD_URL_WORKOUTS = "http://www.viktordebock.be/mad_backend/add/addworkoutbyusername/index.php?";
     public static final String UPLOAD_URL_WORKOUTEXERCISES = "http://www.viktordebock.be/mad_backend/add/addexercisetoworkout/?";
+    public static final String FEED_URL_PLANNER = "http://viktordebock.be/mad_backend/api/plannerworkoutsbyusernameanddate/index.php?username=viktordebock";
 
     private static final int NET_CONNECT_TIMEOUT_MILLIS = 15000;  // 15 seconds
 
@@ -63,6 +58,7 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
         //downloadExercises(contentProviderClient, syncResult);
         downloadWorkouts(contentProviderClient, syncResult);
         //uploadWorkouts(contentProviderClient, syncResult);
+        downloadPlanner(contentProviderClient, syncResult);
     }
 
     private void uploadWorkouts(ContentProviderClient contentProviderClient, SyncResult syncResult){
@@ -281,6 +277,61 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
         Log.d(TAG, "Network synchronization complete");
     }
 
+    private void downloadPlanner(ContentProviderClient contentProviderClient, SyncResult syncResult){
+        Log.d(TAG, "On perform sync is called");
+        Log.d(TAG, "Beginning network synchronization");
+        try {
+            final URL location = new URL(FEED_URL_PLANNER);
+            InputStream stream = null;
+
+            try {
+                Log.d(TAG, "Streaming data from network: " + location);
+
+
+                try {
+                    Log.d(TAG,"Trying to SYNC PLANNER");
+
+                    URL url = new URL(FEED_URL_PLANNER);
+
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setReadTimeout(NET_READ_TIMEOUT_MILLIS);
+                    connection.setConnectTimeout(NET_CONNECT_TIMEOUT_MILLIS);
+                    connection.setRequestMethod("GET");
+                    connection.setRequestProperty("Accept", "application/json");
+                    connection.setRequestProperty("charset", "utf-8");
+                    connection.setDoInput(true);
+
+                    connection.connect();
+
+                    Log.d("","_____________________________response code : "+ connection.getResponseCode());
+                    //Log.d("","_____________________________CONNECTION: " + getStringFromInputStream(connection.getInputStream()));
+                    JsonReader reader = new JsonReader(new InputStreamReader(connection.getInputStream()));
+
+                    parsePlanner(reader, contentProviderClient);
+
+                    connection.disconnect();
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+
+
+            } finally {
+                if (stream != null) {
+                    stream.close();
+                }
+            }
+        } catch (MalformedURLException e) {
+            Log.d(TAG, "Feed URL is malformed", e);
+            syncResult.stats.numParseExceptions++;
+            return;
+        } catch (IOException e) {
+            Log.d(TAG, "Error reading from network: " + e.toString());
+            syncResult.stats.numIoExceptions++;
+            return;
+        }
+        Log.d(TAG, "Network synchronization complete");
+    }
+
     protected void parseExercises(JsonReader reader, ContentProviderClient contentProviderClient) throws Exception {
         int id = -1;
         String name = "";
@@ -458,6 +509,65 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
             } else {
                 contentProviderClient.insert(Contract.Workouts.CONTENT_URI, values);
                 Log.d("", "INSERTED " + id + ";" + name + ";" + isPaid);
+            }
+        }
+
+        reader.endArray();
+    }
+
+    protected void parsePlanner(JsonReader reader, ContentProviderClient contentProviderClient) throws Exception {
+        int id = -1;
+        String wo_date = "";
+        String username = "";
+        int workout_id = 0;
+
+
+        Log.d("","______________DECLARED");
+
+
+        reader.beginObject();   // open {
+        reader.nextName();      // workouts
+        reader.beginArray();    // open [
+
+
+        Log.d("","______________BEGIN ARRAY");
+
+        while (reader.hasNext()){
+            ContentValues values = new ContentValues();
+
+            reader.beginObject();
+
+            reader.nextName();
+            id = reader.nextInt();
+            values.put(Contract.Planners._ID, id);
+
+            reader.nextName();
+            wo_date = reader.nextString();
+            values.put(Contract.Planners.WO_DATE, wo_date);
+
+            reader.nextName();
+            username = reader.nextString();
+
+            reader.nextName();
+            workout_id = reader.nextInt();
+            values.put(Contract.Planners.WORKOUT_ID, workout_id);
+
+            reader.endObject();
+
+            Log.d("", "CONTENTVALUES FORMED " + id + ";" + workout_id + ";" + wo_date);
+
+            Uri uriPlanner = Contract.Planners.ITEM_CONTENT_URI.buildUpon().appendEncodedPath(values.getAsString(Contract.Planners._ID)).build();
+            Cursor cursor = contentProviderClient.query(
+                    uriPlanner,
+                    new String[]{Contract.Planners._ID}, null, null, null);
+
+            if (cursor.getCount() > 0) {
+                values.remove(Contract.Planners._ID);
+                contentProviderClient.update(uriPlanner, values, null, null);
+                Log.d("", "UPDATED " + id + ";" + workout_id + ";" + wo_date);
+            } else {
+                contentProviderClient.insert(Contract.Planners.CONTENT_URI, values);
+                Log.d("", "INSERTED " + id + ";" + workout_id + ";" + wo_date);
             }
         }
 
