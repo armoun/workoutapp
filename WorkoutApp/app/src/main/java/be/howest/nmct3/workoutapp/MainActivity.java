925d6e85ded6354cbe4d51c4b2834763a8cc1eb8
NielsLammens <lammens.niels@gmail.com>
2015-01-19 02:32:41 +0100

    conflict fix

--- WorkoutApp/app/src/main/java/be/howest/nmct3/workoutapp/MainActivity.java
+++ WorkoutApp/app/src/main/java/be/howest/nmct3/workoutapp/MainActivity.java
@@ -69,12 +69,11 @@ public class MainActivity extends FragmentActivity {
 
     public static WorkoutDatasoure workoutDatasource;
 
-<<<<<<< HEAD
     public static Context baseContext;
-=======
+
     public static boolean todaysWorkoutClicked = false;
     public static int currentWorkoutPosition = -1;
->>>>>>> FETCH_HEAD
+
 
     ActionBarDrawerToggle icon;
 
